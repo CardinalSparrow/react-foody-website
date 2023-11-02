88c70d8313f4a7f645b9fac1e5cbaae3fec1b027
Cardinal <Cardinalsparrow@gmail.com>
2023-11-02 03:06:13 +0100

    added faq data file

---
+++ src/Components/Assets/FAQ_data.js
@@ -0,0 +1,18 @@
+export const FAQ_data = [
+    {
+        question: "How to order Pizza?",
+        answer: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quia neque in perspiciatis possimus voluptate delectus veniam consequatur. Ducimus ullam omnis illo dolores doloribus iste voluptates. Temporibus, libero. Quae aliquam rem quos similique dolorum totam officia labore, pariatur, cum sit suscipit id deserunt, molestias repellendus. Adrepudiandae quo architecto!"
+    },
+    {
+        question: "What does different deliveries mean?",
+        answer: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quia neque in perspiciatis possimus voluptate delectus veniam consequatur. Ducimus ullam omnis illo dolores doloribus iste voluptates. Temporibus, libero. Quae aliquam rem quos similique dolorum totam officia labore, pariatur, cum sit suscipit id deserunt, molestias repellendus. Adrepudiandae quo architecto!"
+    },
+    {
+        question: "When do you work?",
+        answer: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quia neque in perspiciatis possimus voluptate delectus veniam consequatur. Ducimus ullam omnis illo dolores doloribus iste voluptates. Temporibus, libero. Quae aliquam rem quos similique dolorum totam officia labore, pariatur, cum sit suscipit id deserunt, molestias repellendus. Adrepudiandae quo architecto!"
+    },
+    {
+        question: "How many dishes do you have?",
+        answer: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quia neque in perspiciatis possimus voluptate delectus veniam consequatur. Ducimus ullam omnis illo dolores doloribus iste voluptates. Temporibus, libero. Quae aliquam rem quos similique dolorum totam officia labore, pariatur, cum sit suscipit id deserunt, molestias repellendus. Adrepudiandae quo architecto!"
+    },
+]
